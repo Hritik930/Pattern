7b26cc99e5c0d9479078317224a3d45a0e29a474
your name <your email>
2026-08-16 19:18:44 +0530

    Pattern solved

--- Solution.java
+++ Solution.java
@@ -1,9 +1,16 @@
 public class Solution {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=i; j++){
-                System.out.print(j);
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+               if(k <=i){
+                    System.out.print(k);
+                }else{
+                    System.out.print(2*i-k);        
+               }
             }
             System.out.println(); 
         }
