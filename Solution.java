17252903a165083fc2b212626625cbe323d77479
your name <your email>
2026-08-16 19:44:56 +0530

    java branch mai

--- Solution.java
+++ Solution.java
@@ -1,13 +1,15 @@
 public class Solution {
+
     public static void main(String[] args) {
         int n = 4;
-        for(char i = 'E'; i>='A'; i--){
-            for(char j = 'E'; j>=i; j--){
-                System.out.print(j + " ");
-            } 
-            
-            System.out.println(); 
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= n - i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
-
