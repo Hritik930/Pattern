2a431c4cb405fabff50e6e95af5ae2f992bacaf1
your name <your email>
2026-08-16 19:37:53 +0530

    problem solved

--- Solution.java
+++ Solution.java
@@ -1,17 +1,11 @@
 public class Solution {
     public static void main(String[] args) {
         int n = 4;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n-i; j++){
-                System.out.print(" ");
-            }
-            for(int k = 1; k <= 2*i-1; k++){
-               if(k <=i){
-                    System.out.print(k);
-                }else{
-                    System.out.print(2*i-k);        
-               }
-            }
+        for(char i = 'E'; i>='A'; i--){
+            for(char j = 'E'; j>=i; j--){
+                System.out.print(j + " ");
+            } 
+            
             System.out.println(); 
         }
     }
